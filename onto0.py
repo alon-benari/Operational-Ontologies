82bbc99f32d7bfa713f722f7a14bca38cf7a5b96
Alon Ben-Ari <alon.benari@gmail.com>
2025-08-20 22:44:02 -0700

    Another commit

--- onto0.py
+++ onto0.py
@@ -1,3 +1,43 @@
 from owlready2 import *
 
-onto = get_ontology("http://lesfleursdunormal.fr/static/_downloads/bacteria.owl").load()
+onto = get_ontology("http://test.org/onto.owl#") 
+
+set_log_level(9)
+
+with onto:
+    class Bacterium(Thing):
+        pass
+    class Shape(Thing):
+        pass    
+    class Group(Thing):
+        pass    
+
+    ## subclasses
+
+    class RodShape(Shape):
+        pass
+
+    class Isolated(Group):
+        pass
+
+    class InPair(Group):
+        pass
+
+    class has_shape(ObjectProperty, FunctionalProperty):
+        domain = [Bacterium]
+        range = [Shape] 
+
+    class has_grouping(Bacterium >> Group, FunctionalProperty):
+        pass
+
+    class gram_positive(Bacterium >> bool, FunctionalProperty):
+        pass    
+
+    ## creating a sub property
+    class has_rare_property(has_shape): 
+        pass
+
+
+## creating individuals
+
+my_bacterium = Bacterium()
